5405f53a494aae79f508dba1e286256c399d4635
Parwinder Singh Batra <64405014+parwindersinghbatra@users.noreply.github.com>
2024-01-10 13:08:49 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+let result = document.getElementById('div_1')
+
+result.style.cssText='width:25%;background-color:yellow;height:200px;margin-top:20px;margin-left:20px;font-weight:bold;font-size:20px;font-color:white;color:blue;padding-top:10px;padding-left:20px'
